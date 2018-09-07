710079254677561430aa01be95bea7d143a72147
施哲晨 <sh081042@gmail.com>
2018-09-07 17:11:29 +0800

    update custom schemes

--- magiccube/ViewController.m
+++ magiccube/ViewController.m
@@ -3,121 +3,105 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()<WKUIDelegate,WKScriptMessageHandler,WKNavigationDelegate>
-    
-    @property (strong, nonatomic)   WKWebView                   *webView;
-    @property (strong, nonatomic)   NSString                   *url;
-    @property (strong, nonatomic)   UIProgressView              *progressView;
-    @end
+
+@property (strong, nonatomic)   WKWebView                   *webView;
+@property (strong, nonatomic)   NSString                   *url;
+@property (strong, nonatomic)   UIProgressView              *progressView;
+@end
 
 @implementation ViewController
-    
+
 - (void)viewDidLoad {
-    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    self.navigationController.navigationBar.barStyle = UIBarStyleDefault;
     
     [self initWKWebView];
-  
+    
 }
-    
-    //初始化页面
+
+//初始化页面
 - (void)initWKWebView
-    {
-        //进行配置控制器
-        WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
-        //实例化对象
-        configuration.userContentController = [WKUserContentController new];
-        
-        [configuration.userContentController addScriptMessageHandler:self name:@"onAuthorizateByexchange"];
-        
-        WKPreferences *preferences = [WKPreferences new];
-        preferences.javaScriptCanOpenWindowsAutomatically = YES;
-        configuration.preferences = preferences;
-        self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:configuration];
-        
-        
-        
-        if (@available(iOS 11.0, *)) {
-            self.webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
-            self.webView.scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
-            self.webView.scrollView.scrollIndicatorInsets = self.webView.scrollView.contentInset;
-        }
-        
-        // 获取默认User-Agent
-        [self.webView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id result, NSError *error) {
-            NSString *oldAgent = result;
-            
-            // 给User-Agent添加额外的信息
-            NSString *newAgent = [NSString stringWithFormat:@"%@;%@", oldAgent, @"APP/JHELLO"];
-            
-            // 设置global User-Agent
-            NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:newAgent, @"UserAgent", nil];
-            [[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
-            [self.webView setCustomUserAgent:newAgent];
-            
-            
-        }];
-        
-        NSString *u1rlStr = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-        
-        NSURL *_url = [NSURL fileURLWithPath:u1rlStr];
-        
-        
-        NSURL *fileURL = _url;
-        
-        [self.webView loadFileURL:fileURL allowingReadAccessToURL:fileURL];
-        
-        self.webView.UIDelegate = self;
-        [self.view addSubview:self.webView];
-        [self disableBounce];
-        
+{
+    //进行配置控制器
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    //实例化对象
+    configuration.userContentController = [WKUserContentController new];
+    
+    WKPreferences *preferences = [WKPreferences new];
+    preferences.javaScriptCanOpenWindowsAutomatically = YES;
+    configuration.preferences = preferences;
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:configuration];
+    
+    if (@available(iOS 11.0, *)) {
+        self.webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        self.webView.scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        self.webView.scrollView.scrollIndicatorInsets = self.webView.scrollView.contentInset;
     }
     
+    // 获取默认User-Agent
+    [self.webView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id result, NSError *error) {
+        NSString *oldAgent = result;
+        NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+         NSString *appVersion = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
+        // 给User-Agent添加额外的信息
+        NSString *newAgent = [NSString stringWithFormat:@"%@;%@", oldAgent, [@"APP/JHLLO app_version:" stringByAppendingString:appVersion]];
+        // 设置global User-Agent
+        NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:newAgent, @"UserAgent", nil];
+        [[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
+        [self.webView setCustomUserAgent:newAgent];
+    }];
+    
+    NSString *urlStr = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSURL *_url = [NSURL fileURLWithPath:urlStr];
+    NSURL *fileURL = _url;
+    [self.webView loadFileURL:fileURL allowingReadAccessToURL:fileURL];
+ 
+//    NSString *urlStr = @"http://exchange.mofangvr.com";
+//    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]];
+//    [self.webView loadRequest:request];
+    
+    self.webView.UIDelegate = self;
+    self.webView.navigationDelegate = self;
+    [self.view addSubview:self.webView];
+    [self disableBounce];
+    
+}
+
 - (BOOL)isOpenAppSpecialURLValue:(NSString *)string
-    {
-        if ([string hasPrefix:@"http://"]) {
-            return NO;
-        } else if ([string hasPrefix:@"https://"]) {
-            return NO;
-        }
-        return YES;
+{
+    if ([string hasPrefix:@"http://"]) {
+        return NO;
+    } else if ([string hasPrefix:@"https://"]) {
+        return NO;
     }
+    return YES;
+}
+
+
 #pragma mark - WKWebView WKNavigationDelegate 相关
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
     // WKWebView默认拦截scheme 需在下面方法手动打开
     // 打开外部应用 Safari等操作
+    
     NSString *_url = navigationAction.request.URL.absoluteString;
     if ([self isOpenAppSpecialURLValue:_url]) { // 对应的scheme
         [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
     }
     decisionHandler(WKNavigationActionPolicyAllow);
 }
-    
+// 页面加载完毕时调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+}
+
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
-    {
-        NSLog(@"body:%@",message.body);
-        NSLog(@"name:%@",message.name);
-        
-        
-        //    [LBS]
-        //      [LBS getLbs];
-        
-        
-        //    if ([message.name isEqualToString:@"closeWeb"]) {
-        //        [self dismissViewControllerAnimated:YES completion:nil];
-        //    }
-        //
-        //    if ([message.name isEqualToString:@"sucShop"]) {
-        //        [self dismissViewControllerAnimated:YES completion:nil];
-        //
-        //        char tmp[255]= {0};
-        //        sprintf(tmp, "cc.vv.anysdkMgr.shop_suc('%d')",-1);
-        //    }
-    }
+{
     
+}
+
 - (BOOL)shouldAutorotate
-    {
-        return true;
-    }
-    
+{
+    return true;
+}
+
 -(void)evaluateJavaScript:(NSString *)string{
     //设置JS
     NSString *inputValueJS = string;
@@ -127,9 +111,7 @@
     }];
 }
 - (void)disableBounce {
-    //    [[[self subviews] lastObject] setScrollingEnabled:NO];
     self.webView.scrollView.bounces = NO;
-    //           self.webView.scrollView.scrollEnabled = NO;
 }
-    @end
+@end
 
